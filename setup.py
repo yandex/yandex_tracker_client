779f28436e6623ba80d3eb629e2f4722e15a4728
smosker <smosker@yandex-team.ru>
2019-07-12 17:31:23 +0300

    Improve description. Issue: COMMONPY-206
    ref:8c470da510836d45b184703e82782097cbc97c15

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 # coding: utf-8
+
+import codecs
+import os
+
 from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
 
 setup(
     name='yandex_tracker_client',
@@ -7,8 +20,20 @@ setup(
     description='Client for Yandex.Tracker',
     author='Yandex Team',
     author_email='smosker@yandex-team.ru',
-    url='https://github.com/yandex/yandex-tracker-client',
+    url='https://github.com/yandex/yandex_tracker_client',
+    long_description=read('README.md'),
     packages=['yandex_tracker_client'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 2',
+                 ],
+    keywords='python yandex.tracker api-client',
+    python_requires='>=2.7',
     install_requires=[
         'requests[security]>=2.0',
         'setuptools',
