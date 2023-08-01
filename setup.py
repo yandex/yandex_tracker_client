35b8b3fcef1135d093edf2c1853dad9b0dd795f1
smosker <smosker@yandex-team.com>
2023-08-01 23:33:51 +0300

    releasing version 2.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(fname):
 
 setup(
     name='yandex_tracker_client',
-    version='2.5',
+    version='2.6',
     description='Client for Yandex.Tracker',
     author='Yandex Team',
     author_email='smosker@yandex-team.ru',
